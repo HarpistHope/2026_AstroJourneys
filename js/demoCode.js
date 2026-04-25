f1c3ded16beca2f44c33032046be2559c83e7a7c
Tiffany Jones <tajones7@wisc.edu>
2026-04-25 14:54:49 -0500

    more timeline and image updates

--- js/demoCode.js
+++ js/demoCode.js
@@ -100,7 +100,7 @@
         year: 1961, // complete date May 5, 1961
         distance: 303, //duration: 00:15:28 HMS
         coords: [-80.6, 28.5], // Cape Canaveral Space Force Station (lng, lat) 
-        location: "Cape Canaveral Space Force Station", // Cape Canaveral Space Force Station
+        location: "Cape Canaveral Air Force Station", // now called Cape Canaveral Space Force Station"
         locDateFounded: "",
         locImage: "Mercury_AlanShepard_Freedom7Capsule_1961.jpg", //photo info copied from NASA site: Astronaut Alan B. Shepard Jr., in his pressure suit and helmet, is being inserted into the Freedom 7 capsule in preparation for the Mercury-Redstone 3 (MR-3) mission
         infoImage: "Mercury_Earth_MercuryRedstone3_1961.jpg", //photo info from NASA site: Mercury-Redstone 3 (MR-3) spaceflight Earth observations of a cloudy Earth surface.
@@ -112,7 +112,7 @@
         year: 1961-1963,
         distance: "",
         coords: [-80.6, 28.5], // Cape Canaveral Space Force Station (lng, lat) 
-        location: "Cape Canaveral Space Force Station",
+        location: "Cape Canaveral Air Force Station", // now called Cape Canaveral Space Force Station"
         locDateFounded: "",
         locImage: "Mercury_MissionControl_1962.jpg", //Copied from NASA: Flight controllers gather inside Mercury Mission Control during the first orbit of John Glenn's Friendship 7 mission, which launched on Feb. 20, 1962.
         infoImage: "Mercury_ScottCarpenterTraining_1961.jpg", //from NASA: Project Mercury astronaut M. Scott Carpenter, prime pilot for the United States second manned orbital flight, undergoes a simulated mission in the procedures trainer at Langley Air Force Base, Virginia, headquarters for the National Aeronautics and Space Administration?s Manned Spacecraft Center.
@@ -122,51 +122,50 @@
         title: "Mercury-Atlas 6: Friendship 7 Mission",
         description: "Astronaut John Glenn made history onboard the Friendship 7 Mercury spacecraft, becoming the first U.S. astronaut to orbit Earth.",
         year: 1962, // complete date February 20, 1962
-        distance: 75679, //duration: 04:55:23 HMS
+        distance: 75679, //duration: 04:55:23 HMS, altitude: 162.2 x 100 statute miles
         coords: [-80.6, 28.5], // Cape Canaveral Space Force Station (lng, lat) 
-        location: "Cape Canaveral Space Force Station",
+        location: "Cape Canaveral Air Force Station", // now called Cape Canaveral Space Force Station"
         locDateFounded: "",
         locImage: "Mercury_Atlas6_1962.jpg", //caption copied from NASA: This is a view of the Mercury-Atlas 6 spacecraft as it leaves the launch pad at Cape Canaveral, Florida. Onboard the spacecraft is astronaut John H. Glenn Jr., pilot of the MA-6 mission, on its way to a three-Earth-orbital mission, making Glenn the first American to fly a manned Earth-orbiting mission.
-        infoImage: "",
-        funFact:""
+        infoImage: "Mercury_JohnGlennAtlas6_1962.jpg", //from NASA: A camera onboard the "Friendship 7" Mercury spacecraft photographs astronaut John H. Glenn Jr. during the Mercury-Atlas 6 spaceflight
+        funFact:"After 10 postponements due to inclement weather and mechanical problems, John Glenn reached orbit a mere five minutes, one second after countdown."
     },
     {
-        title: "Gemini IV Mission: First Walk in Space",
+        title: "Gemini IV: First Walk in Space",
         description: "NASA astronaut Ed White became the first American to walk in space.",
         year: 1965, // complete date June 3, 1965
         distance: 1600000, //duration: 97:56:12 HMS
-        coords: [-80.6, 28.5], // (lng, lat)
-        location: "Cape Canaveral",
+        coords: [-82.4, 36.3], // Houston, TX (lng, lat)
+        location: "Manned Spacecraft Center (MSC) in Houston", // copied from NASA: Gemini 4 was the first flight to be followed by the mission control at the new Manned Spacecraft Center (MSC) in Houston and became the primary flight control center for all subsequent U.S. manned space missions from Project Gemini forward. (now called NASA Lyndon B. Johnson Space Center (JSC)) 
         locDateFounded: "",
-        locImage: "",
-        infoImage: "",
-        funFact: ""
-        // funFact2: "Project Gemini expanded onboard capacity to two astronauts, spanning 10 manned missions."
+        locImage: "Gemini_MissionControlHouston_1965.jpg", //from NASA: The families of Gemini 4 astronauts James A. McDivitt and Edward H. White II visited the Mission Control Center in Houston. In the foreground, left to right, are Mrs. Patricia McDivitt, daughter Bonnie White, Mrs. Patricia White, flight director Christopher C. Kraft Jr., and Edward White III. Each of the family members talked with the astronauts as they passed over the United States.
+        infoImage: "Gemini_EdwardWhiteGeminiTitan4_1965.jpg", //copied from NASA: Astronaut Edward H. White II, pilot of the Gemini IV four-day Earth-orbital mission, floats in the zero gravity of space outside the Gemini IV spacecraft. He remained outside the spacecraft for 21 minutes during the third revolution of the Gemini IV mission. White wears a specially designed spacesuit; and the visor of the helmet is gold plated to protect him against the unfiltered rays of the sun. He wears an emergency oxygen pack, also. He is secured to the spacecraft by a 25-feet umbilical line and a 23-feet tether line, both wrapped in gold tape to form one cord. In his right hand is a Hand-Held Self-Maneuvering Unit (HHSMU) with which he controls his movements in space.
+        funFact: "Gemini IV was the first flight monitored by mission control at the new Manned Spacecraft Center in Houston, Texas rather than from its previous location at Cape Canaveral."
+        // funFact2: "Project Gemini expanded onboard capacity to two astronauts, making Jim McDivitt and Ed White the first duo of astronauts in space, chasing the Soviet Union cosmonaut Alexei Leonov's successful spacewalk a mere three months prior to their mission."
     },
     {
         title: "Apollo 1: Our First Loss",
         description: "The Apollo 1 Mission marked the first tragedy of spaceflight as fire broke out during a preflight test, taking the lives of Astronauts Gus Grissom, Ed White, and Roger Chaffee, fueling a renewed dedication to safety protocol.",
         year: 1967, // complete date Jan. 27, 1967
-        distance: , //duration: 
+        distance: "", //duration: 
         coords: [-80.6, 28.5], // (lng, lat)
-        location: "Cape Canaveral",
+        location: "Cape Canaveral Air Force Station", // now called Cape Canaveral Space Force Station"
         locDateFounded: "",
         locImage: "",
-        infoImage: "",
+        infoImage: "Apollo_Apollo1Crew_1966", //Astronauts Grissom, White and Chaffee 
         funFact: ""
     },
     {
-        title: "Messages from Space",
+        title: "Apollo 7: Messages from Space",
         description: "The first live TV broadcast from a manned U.S. spacecraft",
-        year: 1968, // complete date Oct. 19, 1968
-        distance: , //duration: 
+        year: 1968, // complete date Oct. 14, 1968
+        distance: "", //duration: 
         coords: [], // (lng, lat)
         location: "",
         locDateFounded: "",
         locImage: "",
-        infoImage: "",
-        funFact:""
-
+        infoImage: "Apollo_Apollo7Broadcast_1968", //from NASA: Astronauts Walter M. Schirra Jr. (on right), mission commander; and Donn F. Eisele, command module pilot; are seen in the first live television transmission from space.
+        funFact:"For a broadcast to be possible, the spacecraft needed to be passing over the continental United States, since only the ground stations in Corpus Christi, Texas, and Merritt Island, Florida, had the equipment to receive and convert the signals for broadcasting. Over the course of the mission, the crew made three jovial broadcasts, giving people on Earth a tour of the spacecraft and demonstrated the challenges of food prep in space."
     },
     {
         title: "Apollo 11: One Giant Leap for Mankind",
@@ -180,19 +179,22 @@
         infoImage: "apollo11.jpg", 
         funFact: "The Other Astronaut: Michael Collins had a vital role to play - but he often gets forgotten. While his buddies Neil and Buzz were exploring the lunar surface for the first time, Michael stayed in lunar orbit to pilot the command module."
         // think about adding linked image and map view for each event here
+        // cool photo of parade after Apollo 11 mission "Apollo_Apollo11Parade_1969.jpg" From NASA: New York City welcomes Apollo 11 crewmen in a showering of ticker tape down Broadway and Park Avenue in a parade termed as the largest in the city's history. Pictured in the lead car, from the right, are astronauts Neil A. Armstrong, commander; Michael Collins, command module pilot; and Edwin E. Aldrin Jr., lunar module pilot.
     },
     {
         title: "Apollo 13: Beating the Odds",
         description: "",
         year: 1970, //complete year: April 17, 1970 
-        distance: , //duration: 
+        distance: "", //duration: 
         coords: [], // (lng, lat)
         location: "",
         locDateFounded: "",
-        locImage: "",
-        infoImage: "", 
-        funFact: "The Apollo 13 crew used ingenuity to configure onboard equipment and supplies to successfully remove carbon dioxide from the Lunar Module after an oxygen tank explosion, returning to Earth safely."
-    },
+        locImage: "Apollo_Apollo13Recovery_1970.jpg", //Crew men aboard the USS Iwo Jima recovery ship hoist the Command Module (CM) aboard ship in the South Pacific Ocean.
+        // other photo option: "Apollo_Apollo13CrewRecovery_1970.jpg" crew on recovery ship
+        infoImage: "Apollo_Apollo13Moon_1970.jpg", //This view of the moon captured from the Apollo 13 spacecraft shows the Moon's Sea of Crisis, the Sea of Fertility, the Sea of Tranquility, the Sea of Serenity, the Sea of Nectar, the Sea of Vapors, the Border Sea, Smyth's Sea, the crater Langrenus, and the crater Tsiolkovsky.
+        // other photo options: Apollo_Apollo13SpaceModule_1970.jpg damaged Service Module photographed from the Lunar Module
+        funFact: "The Apollo 13 crew used ingenuity to configure onboard equipment and supplies to successfully remove carbon dioxide from the Lunar Module after an oxygen tank explosion in the Service Module, using the Lunar Module as a lifeboat to return to Earth safely."
+    }, 
     {
         title: "Artemis II",
         description: "Farthest distance from Earth",
